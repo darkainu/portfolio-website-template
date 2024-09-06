04c6cb59189e0e41b2354842567783878d33267f
darkainu <darqainu@gmail.com>
2024-09-06 05:19:02 -0700

    s

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/website",
-  assetPrefix: "/website/",
+  output: 'export',
+  basePath: '/website',
+  assetPrefix: '/website/',
   images: {
     unoptimized: true,
   },
-};
+}
 
-module.exports = nextConfig;
+export default nextConfig

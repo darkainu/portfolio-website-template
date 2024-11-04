be72554c7000c5f407ed696314b51e1728d69729
darkainu <darqainu@gmail.com>
2024-11-04 15:25:34 -0800

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import HeaderWrapper from './components/HeaderWrapper'
+import HeaderWrapper from '@/components/HeaderWrapper'
 
 const inter = Inter({ subsets: ['latin'] })
 
